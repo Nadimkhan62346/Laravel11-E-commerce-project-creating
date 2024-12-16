216abc832f8f3ebcfaf08778621a935ddf9f1b6a
Nadimkhan62346 <nadimuddin62346@gmail.com>
2024-12-16 19:19:21 +0600

    creating admin for new brand section

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -9,5 +11,14 @@ class AdminController extends Controller
     public function index()
     {
         return view('Admin.index');
+    }
+    public function brands()
+    {
+        $brands = Brand::orderBy('id','DESC')->paginate(10);
+        return view('Admin.brands',compact('brands'));
+    }
+    public function add_brand()
+    {
+        return view('admin.add-brand');
     }
 }
