41403f56bd19200906e90df2a70f009a3f616369
Nadimkhan62346 <nadimuddin62346@gamil.com>
2025-01-03 18:33:18 +0600

    update some issue

--- resources/views/Admin/product-edit.blade.php
+++ resources/views/Admin/product-edit.blade.php
@@ -9,13 +9,17 @@
                 <h3>Add Product</h3>
                 <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
                     <li>
-                        <a href="index.html"><div class="text-tiny">Dashboard</div></a>
+                        <a href="index.html">
+                            <div class="text-tiny">Dashboard</div>
+                        </a>
                     </li>
                     <li>
                         <i class="icon-chevron-right"></i>
                     </li>
                     <li>
-                        <a href="all-product.html"><div class="text-tiny">Products</div></a>
+                        <a href="all-product.html">
+                            <div class="text-tiny">Products</div>
+                        </a>
                     </li>
                     <li>
                         <i class="icon-chevron-right"></i>
@@ -26,20 +30,23 @@
                 </ul>
             </div>
             <!-- form-add-product -->
-            <form class="tf-section-2 form-add-product" method="POST" enctype="multipart/form-data" action="{{route('admin.product.update')}}" >
-                <input type="hidden" name="id" value="{{$product->id}}" />
+            <form class="tf-section-2 form-add-product" method="POST" enctype="multipart/form-data"
+                action="{{ route('admin.product.update') }}">
+                <input type="hidden" name="id" value="{{ $product->id }}" />
                 @csrf
-                @method("PUT")
+                @method('PUT')
                 <div class="wg-box">
                     <fieldset class="name">
                         <div class="body-title mb-10">Product name <span class="tf-color-1">*</span></div>
-                        <input class="mb-10" type="text" placeholder="Enter product name" name="name" tabindex="0" value="{{$product->name}}" aria-required="true" required="">
+                        <input class="mb-10" type="text" placeholder="Enter product name" name="name" tabindex="0"
+                            value="{{ $product->name }}" aria-required="true" required="">
                         <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
                     </fieldset>
 
                     <fieldset class="name">
                         <div class="body-title mb-10">Slug <span class="tf-color-1">*</span></div>
-                        <input class="mb-10" type="text" placeholder="Enter product slug" name="slug" tabindex="0" value="{{$product->slug}}" aria-required="true" required="">
+                        <input class="mb-10" type="text" placeholder="Enter product slug" name="slug" tabindex="0"
+                            value="{{ $product->slug }}" aria-required="true" required="">
                         <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
                     </fieldset>
 
@@ -50,7 +57,9 @@
                                 <select class="" name="category_id">
                                     <option>Choose category</option>
                                     @foreach ($categories as $category)
-                                    <option value="{{$category->id}}" {{$product->category_id == $category->id ? "selected":""}}>{{$category->name}}</option>
+                                        <option value="{{ $category->id }}"
+                                            {{ $product->category_id == $category->id ? 'selected' : '' }}>
+                                            {{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -61,7 +70,9 @@
                                 <select class="" name="brand_id">
                                     <option>Choose Brand</option>
                                     @foreach ($brands as $brand)
-                                    <option value="{{$brand->id}}" {{$product->brand_id == $brand->id ? "selected":""}}>{{$brand->name}}</option>
+                                        <option value="{{ $brand->id }}"
+                                            {{ $product->brand_id == $brand->id ? 'selected' : '' }}>{{ $brand->name }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -70,13 +81,15 @@
 
                     <fieldset class="shortdescription">
                         <div class="body-title mb-10">Short Description <span class="tf-color-1">*</span></div>
-                        <textarea class="mb-10 ht-150" name="short_description" placeholder="Short Description" tabindex="0" aria-required="true" required="">{{$product->short_description}}</textarea>
+                        <textarea class="mb-10 ht-150" name="short_description" placeholder="Short Description" tabindex="0"
+                            aria-required="true" required="">{{ $product->short_description }}</textarea>
                         <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
                     </fieldset>
 
                     <fieldset class="description">
                         <div class="body-title mb-10">Description <span class="tf-color-1">*</span></div>
-                        <textarea class="mb-10" name="description" placeholder="Description" tabindex="0" aria-required="true" required="">{{$product->description}}</textarea>
+                        <textarea class="mb-10" name="description" placeholder="Description" tabindex="0" aria-required="true"
+                            required="">{{ $product->description }}</textarea>
                         <div class="text-tiny">Do not exceed 100 characters when entering the product name.</div>
                     </fieldset>
                 </div>
@@ -84,17 +97,19 @@
                     <fieldset>
                         <div class="body-title">Upload images <span class="tf-color-1">*</span></div>
                         <div class="upload-image flex-grow">
-                            @if($product->image)
-                            <div class="item" id="imgpreview">
-                                <img src="{{asset('uploads/products')}}/{{$product->image}}" class="effect8" alt="">
-                            </div>
+                            @if ($product->image)
+                                <div class="item" id="imgpreview">
+                                    <img src="{{ asset('uploads/products') }}/{{ $product->image }}" class="effect8"
+                                        alt="">
+                                </div>
                             @endif
                             <div id="upload-file" class="item up-load">
                                 <label class="uploadfile" for="myFile">
                                     <span class="icon">
                                         <i class="icon-upload-cloud"></i>
                                     </span>
-                                    <span class="body-text">Drop your images here or select <span class="tf-color">click to browse</span></span>
+                                    <span class="body-text">Drop your images here or select <span class="tf-color">click to
+                                            browse</span></span>
                                     <input type="file" id="myFile" name="image" accept="image/*">
                                 </label>
                             </div>
@@ -104,20 +119,22 @@
                     <fieldset>
                         <div class="body-title mb-10">Upload Gallery Images</div>
                         <div class="upload-image mb-16">
-                            @if($product->images)
-                                @foreach(explode(",",$product->images) as $img)
+                            @if ($product->images)
+                                @foreach (explode(',', $product->images) as $img)
                                     <div class="item gitems">
-                                        <img src="{{asset('uploads/products')}}/{{trim($img)}}" class="effect8" alt="">
+                                        <img src="{{ asset('uploads/products') }}/{{ trim($img) }}" class="effect8"
+                                            alt="">
                                     </div>
                                 @endforeach
                             @endif
 
-                            <div  id ="galUpload" class="item up-load">
+                            <div id ="galUpload" class="item up-load">
                                 <label class="uploadfile" for="gFile">
                                     <span class="icon">
                                         <i class="icon-upload-cloud"></i>
                                     </span>
-                                    <span class="text-tiny">Drop your images here or select <span class="tf-color">click to browse</span></span>
+                                    <span class="text-tiny">Drop your images here or select <span class="tf-color">click
+                                            to browse</span></span>
                                     <input type="file" id="gFile" name="images[]" accept="image/*" multiple>
                                 </label>
                             </div>
@@ -127,11 +144,14 @@
                     <div class="cols gap22">
                         <fieldset class="name">
                             <div class="body-title mb-10">Regular Price <span class="tf-color-1">*</span></div>
-                            <input class="mb-10" type="text" placeholder="Enter regular price" name="regular_price" tabindex="0" value="{{$product->regular_price}}" aria-required="true" required="">
+                            <input class="mb-10" type="text" placeholder="Enter regular price" name="regular_price"
+                                tabindex="0" value="{{ $product->regular_price }}" aria-required="true"
+                                required="">
                         </fieldset>
                         <fieldset class="name">
                             <div class="body-title mb-10">Sale Price <span class="tf-color-1">*</span></div>
-                            <input class="mb-10" type="text" placeholder="Enter sale price" name="sale_price" tabindex="0" value="{{$product->sale_price}}" aria-required="true" required="">
+                            <input class="mb-10" type="text" placeholder="Enter sale price" name="sale_price"
+                                tabindex="0" value="{{ $product->sale_price }}" aria-required="true" required="">
                         </fieldset>
                     </div>
 
@@ -139,11 +159,13 @@
                     <div class="cols gap22">
                         <fieldset class="name">
                             <div class="body-title mb-10">SKU <span class="tf-color-1">*</span></div>
-                            <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU" tabindex="0" value="{{$product->SKU}}" aria-required="true" required="">
+                            <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU" tabindex="0"
+                                value="{{ $product->SKU }}" aria-required="true" required="">
                         </fieldset>
                         <fieldset class="name">
                             <div class="body-title mb-10">Quantity <span class="tf-color-1">*</span></div>
-                            <input class="mb-10" type="text" placeholder="Enter quantity" name="quantity" tabindex="0" value="{{$product->quantity}}" aria-required="true" required="">
+                            <input class="mb-10" type="text" placeholder="Enter quantity" name="quantity"
+                                tabindex="0" value="{{ $product->quantity }}" aria-required="true" required="">
                         </fieldset>
                     </div>
 
@@ -152,8 +174,10 @@
                             <div class="body-title mb-10">Stock</div>
                             <div class="select mb-10">
                                 <select class="" name="stock_status">
-                                    <option value="instock" {{$product->stock_status == "instock" ? "Selected":"" }}>InStock</option>
-                                    <option value="outofstock"  {{$product->stock_status == "outofstock" ? "Selected":"" }}>Out of Stock</option>
+                                    <option value="instock" {{ $product->stock_status == 'instock' ? 'Selected' : '' }}>
+                                        InStock</option>
+                                    <option value="outofstock"
+                                        {{ $product->stock_status == 'outofstock' ? 'Selected' : '' }}>Out of Stock</option>
                                 </select>
                             </div>
                         </fieldset>
@@ -161,8 +185,8 @@
                             <div class="body-title mb-10">Featured</div>
                             <div class="select mb-10">
                                 <select class="" name="featured">
-                                    <option value="0" {{$product->featured == "0" ? "Selected":"" }}>No</option>
-                                    <option value="1" {{$product->featured == "1" ? "Selected":"" }}>Yes</option>
+                                    <option value="0" {{ $product->featured == '0' ? 'Selected' : '' }}>No</option>
+                                    <option value="1" {{ $product->featured == '1' ? 'Selected' : '' }}>Yes</option>
                                 </select>
                             </div>
                         </fieldset>
@@ -179,39 +203,45 @@
     <!-- /main-content-wrap -->
 @endsection
 
-@push("scripts")
+
+@push('scripts')
     <script>
-            $(function(){
-                $("#myFile").on("change",function(e){
-                    const photoInp = $("#myFile");
-                    const [file] = this.files;
-                    if (file) {
-                        $("#imgpreview img").attr('src',URL.createObjectURL(file));
-                        $("#imgpreview").show();
-                    }
-                });
-
-
-                $("#gFile").on("change",function(e){
-                    $(".gitems").remove();
-                    const gFile = $("gFile");
-                    const gphotos = this.files;
-                    $.each(gphotos,function(key,val){
-                        $("#galUpload").prepend(`<div class="item gitems"><img src="${URL.createObjectURL(val)}" alt=""></div>`);
-                    });
-                });
-
-
-                $("input[name='name']").on("change",function(){
-                    $("input[name='slug']").val(StringToSlug($(this).val()));
-                });
-
+        $(function() {
+            $("#myFile").on("change", function() {
+                const file = this.files[0];
+                if (file) {
+                    $("#imgpreview img").attr('src', URL.createObjectURL(file));
+                    $("#imgpreview").show();
+                }
             });
-            function StringToSlug(Text) {
-                return Text.toLowerCase()
-                .replace(/[^\w ]+/g, "")
-                .replace(/ +/g, "-");
-            }
+
+            $("#gFile").on("change", function() {
+                $(".gitems").remove();
+                const gphotos = this.files;
+                if (gphotos.length > 0) {
+                    $.each(gphotos, function(key, val) {
+                        if (val) {
+                            $("#galUpload").before( <
+                                div class = "item gitems" > < img src =
+                                "${URL.createObjectURL(val)}"
+                                alt = "Gallery Image" > < /div>
+                            );
+                        }
+                    });
+                }
+            });
+
+            $("input[name='name']").on("change", function() {
+                $("input[name='slug']").val(StringToSlug($(this).val()));
+            });
+        });
+
+        function StringToSlug(Text) {
+            return Text.toLowerCase()
+                .replace(/[^\w\s-]/g, '')
+                .replace(/\s+/g, '-')
+                .replace(/^-+|-+$/g, '');
+        }
     </script>
 @endpush
 
